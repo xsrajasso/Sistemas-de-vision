6ceee980654467c32a44da530979f1b69506f86d
Tomas Jasso <62636466+xsrajasso@users.noreply.github.com>
2023-03-01 16:06:06 -0600

    practica 3
    cambios en los ejemplos

--- Laboratorio/practica3/segmentacion.py
+++ Laboratorio/practica3/segmentacion.py
@@ -1,10 +1,14 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('hoja.jpg')
+img = cv2.imread('cel.jpg')
 hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 bound_lower = np.array([36, 25, 25])
 bound_upper = np.array([70, 255,255 ])
+
+#values for red masks ----------------------------------
+#bound_lower = np.array([0, 50, 50])
+#bound_upper = np.array([10, 255,255 ])
 
 mask_green = cv2.inRange(hsv_img, bound_lower, bound_upper)
 kernel = np.ones((7,7),np.uint8)
@@ -16,6 +20,7 @@ seg_img = cv2.bitwise_and(img, img, mask=mask_green)
 contours, hier = cv2.findContours(mask_green.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 output = cv2.drawContours(seg_img, contours, -1, (0, 0, 255), 3)
 
+cv2.imshow("Originalimage", img)
 cv2.imshow("Result", seg_img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
