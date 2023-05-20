21191deea14ade17384b836e9ee392047125779a
Tomas Jasso <62636466+xsrajasso@users.noreply.github.com>
2023-05-19 18:31:45 -0600

    practica8

--- Laboratorio/practica8/cascade.py
+++ Laboratorio/practica8/cascade.py
@@ -11,25 +11,15 @@ def imshow(title = "Image", image = None, size = 10):
     plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
     plt.title(title)
     plt.show()
-'''
-# Descargamos y descomprimimos el clasificador e imagenes
-!wget https://moderncomputervision.s3.eu-west-2.amazonaws.com/images.zip
-!wget https://moderncomputervision.s3.eu-west-2.amazonaws.com/haarcascades.zip
-
-!unzip -qq images.zip
-!unzip -qq haarcascades.zip
-
-'''
 
 def faces():
-
+    
     # Usamos la funcio CascadeClassifier 
     # El archivo XML del clasificador se guarda en una variable
     face_classifier = cv2.CascadeClassifier('Haarcascades/haarcascade_frontalface_default.xml')
 
     # Cargamos nuestra imagen y la convertimos a escala de grises
-    #image = cv2.imread('images/yo.jpg')
-    cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)
+    image = cv2.imread('images/yo.jpg')
     imshow('Imagen Original', image)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
@@ -39,7 +29,7 @@ def faces():
     faces = face_classifier.detectMultiScale(gray, scaleFactor = 1.3, minNeighbors = 5)
 
     # Si no se detecta niguna cara al clasificador regresa una Tupla vacia
-    if faces == ():
+    if faces is ():
         print("No faces found")
 
     #Iteramos sobre las coordenadas y dibujamos un rectangulo sobre cada cara detectada
@@ -48,7 +38,8 @@ def faces():
 
     imshow('Face Detection', image)
 
-def faceEyes():
+def facesEyes():
+    
     import numpy as np
     import cv2
     
@@ -57,13 +48,13 @@ def faceEyes():
     
     img = cv2.imread('images/yo.jpg')
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    
+
     faces = face_classifier.detectMultiScale(gray, 1.3, 5)
-    
+
     # When no faces detected, face_classifier returns and empty tuple
-    if faces == ():
+    if faces is ():
         print("No Face Found")
-    
+
     for (x,y,w,h) in faces:
         cv2.rectangle(img,(x,y),(x+w,y+h),(127,0,255),2)
         roi_gray = gray[y:y+h, x:x+w]
@@ -71,9 +62,8 @@ def faceEyes():
         eyes = eye_classifier.detectMultiScale(roi_gray, 1.2, 3)
         for (ex,ey,ew,eh) in eyes:
             cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(255,255,0),2)
-    
+
     imshow('Eye & Face Detection',img)
 
-
-faces()
-#faceEyes()
+#faces()
+facesEyes()
